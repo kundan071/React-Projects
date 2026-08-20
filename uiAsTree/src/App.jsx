1e47053b3e9b8a53c07a3320d7c5a0137cc36489
Kundan Patil <kundancpatil2000@gmail.com>
2026-08-20 22:51:18 +0530

    Add uiAsTree React project

---
+++ uiAsTree/src/App.jsx
@@ -0,0 +1,13 @@
+import FancyText from './FancyText';
+import InspirationGenerator from './Generator';
+
+
+export default function App() {
+  return (
+    <>
+      <FancyText title text="Get Inspired App" />
+      <InspirationGenerator />
+
+    </>
+  );
+}
